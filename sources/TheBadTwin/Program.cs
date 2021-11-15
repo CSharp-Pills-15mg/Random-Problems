9c53c64877a254cc01ad1a3e430fbd48d4eebd05
Alexandru Iuga <alex.iuga@gmail.com>
2021-11-15 23:04:16 +0200

    first commit

---
+++ sources/TheBadTwin/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace DustInTheWind.TheBadTwin
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Demo demo = new Demo();
+            await demo.Execute();
+        }
+    }
+}
