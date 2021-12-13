72b48be2bfd0c5069221fb50b1a56acce1de6113
Alexandru Iuga <alex.iuga@gmail.com>
2021-12-13 18:28:55 +0200

    added solution using the ThreadLocal class.

--- sources/RandomProblems.Multithreading/Business/RandomNumbers/RandomNumbersList.cs
+++ sources/RandomProblems.Multithreading/Business/RandomNumbers/RandomNumbersList.cs
@@ -17,21 +17,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace DustInTheWind.RandomProblems.Business.RandomNumbers
 {
     internal class RandomNumbersList : IEnumerable<int>
     {
-        private static readonly Random Random = new();
+        private static readonly Random SeedRandom = new();
+
+        /// <summary>
+        /// This is a more recent solution. <see cref="ThreadLocal{T}"/> is available from .NET Framework 4.0.
+        /// Behind the scene it is automatically creating a single instance of the `Random` class for each thread that requests it.
+        /// </summary>
+        private static readonly ThreadLocal<Random> ThreadLocalRandom = new(CreateNewRandom);
+
         private readonly List<int> numbers = new();
-        
+
         public int this[int index] => numbers[index];
 
         public int GenerateNext()
         {
-            int number = Random.Next();
+            // The `Value` property returns the `Random` instance specific to the current thread.
+            int number = ThreadLocalRandom.Value.Next();
             numbers.Add(number);
             return number;
+        }
+
+        private static Random CreateNewRandom()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Creating Random instance for thread {threadId}");
+
+            int seed = GenerateNewSeed();
+            return new Random(seed);
+        }
+
+        private static int GenerateNewSeed()
+        {
+            lock (SeedRandom)
+            {
+                // We use a separate `Random` instance (called here `SeedRandom`) only for generating the seed
+                // for the real `Random` instance.
+                return SeedRandom.Next();
+            }
         }
 
         public IEnumerator<int> GetEnumerator()
